8cd7366c0a3be64e4006c2bdee2524eb545abaeb
HuuuWasabe <nilgabriel365@gmail.com>
2025-11-18 20:45:48 +0800

    New Commit - Fixed menu bar - Added sidebar with contents - Added bio page

--- htdocs/assets/js/dropdown.js
+++ htdocs/assets/js/dropdown.js
@@ -2,13 +2,33 @@
 document.addEventListener('DOMContentLoaded', () => {
   const toggle = document.querySelector('.dropdown-toggle');
   const menu = document.querySelector('.dropdown-menu');
+  const sidebarSection = document.getElementById('sidebar-section');
 
   toggle.addEventListener('click', (e) => {
     e.preventDefault();
     const expanded = toggle.getAttribute('aria-expanded') === 'true';
     toggle.setAttribute('aria-expanded', !expanded);
     menu.classList.toggle('show');
+    sidebarSection.classList.toggle('active');
   });
+
+  // Sidebar
+  const sidebarItems = document.querySelectorAll('.sidebar-item');
+  const contentBoxes = document.querySelectorAll('.sidebar-content-box');
+
+  sidebarItems.forEach(item => {
+    item.addEventListener('click', () =>{
+       // Remove old active
+       sidebarItems.forEach(i => i.classList.remove('active'));
+       contentBoxes.forEach(b => b.classList.remove('active'));
+
+       // Renew Active
+       item.classList.add('active');
+       const target = item.getAttribute('data-target');
+       document.getElementById(target).classList.add('active');
+       })
+    })
+
 
   // Close when clicking outside
   document.addEventListener('click', (e) => {
